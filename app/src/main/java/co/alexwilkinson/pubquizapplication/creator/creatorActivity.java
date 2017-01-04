5218aff8d903ac1d25800e2a4fb65bcaf4db8da0
anontyro <alexwilkinson@gmail.com>
2017-01-04 12:36:14 +0800

    Initial commit

---
+++ app/src/main/java/co/alexwilkinson/pubquizapplication/creator/creatorActivity.java
@@ -0,0 +1,15 @@
+package co.alexwilkinson.pubquizapplication.creator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.alexwilkinson.pubquizapplication.R;
+
+public class creatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creator);
+    }
+}
---
+++ app/src/main/java/co/alexwilkinson/pubquizapplication/util/dbconnect.java
@@ -0,0 +1,11 @@
+package co.alexwilkinson.pubquizapplication.util;
+
+/**
+ * Created by Alex on 04/01/2017.
+ */
+
+public class dbconnect {
+
+
+
+}
